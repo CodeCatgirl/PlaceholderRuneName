81b99554979bd7a4bbd06a1841ccc14b221ab777
CodeCatgirl <46618618+CodeCatgirl@users.noreply.github.com>
2022-10-22 09:25:20 -0600

    I dnt even remember tbh Please enter the commit message for your changes. Lines starting

--- PlaceholderRuneName/src/main/java/io/github/codecatgirl/placeholderrunename/placeholderrunename.java
+++ PlaceholderRuneName/src/main/java/io/github/codecatgirl/placeholderrunename/placeholderrunename.java
@@ -3,5 +3,13 @@ package io.github.codecatgirl.placeholderrunename;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class placeholderrunename extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Runic Enchants has been enabled!");
+	}
 
+	@Override
+	public void onDisable() {
+		getLogger().info("Runic Enchants has been disabled.");
+	}
 }
